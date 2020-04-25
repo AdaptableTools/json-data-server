224dd8db295553376ad2a56dfcad73537b79a2e4
roblotter <roblotter1@gmail.com>
2020-04-25 15:09:21 +0300

    update cors

--- json-server/app.js
+++ json-server/app.js
@@ -27,7 +27,7 @@ module.exports = function expressApp() {
   app.use("/", router);
 
   // Apply express middlewares
-  app.use(cors());
+  router.use(cors());
 
   router.use(bodyParser.json());
   router.use(bodyParser.urlencoded({ extended: true }));
